a640eb08c6909bb6a35e5ff95f7e5ddec66d0cf4
lidicedupuit <lidicedupuit2710@gmail.com>
2023-04-15 17:23:50 -0300

    alterando  html e js

--- main.js
+++ main.js
@@ -8,12 +8,15 @@ $('#botao-cancelar').click(function(){
 
 $('form').on('submit',function(e) {
     e.preventDeFault();
-    const endererecoDaNovaImagem =$('#endereco-imagem-nova').Val();
-    const novoItem = $('<li class="li"style="display:none"></li>');
-    $('<img src="${enderecoDaImagem}"/>').appendTo(novoItem);
-    $(`<div class="overlay-imagem-link">+
-    '<a href="'+enderecoNovaImagem + "'target="_blank" title="ver imagem em tamnaho real" class="a">ver imagem em tamnho real'</div>`
-).appendTo(novoItem);
+    const endererecoDaNovaImagem =$("#endereco-imagem-nova").Val();
+    const novoItem = $('<li style="display:none"></li>');
+    $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem);
+    $(`<div class="overlay-imagem-link"> 
+    <a href="${enderecoDaNovaImagem}"  "target="_blank" title="ver imagem em tamnaho real">
+        ver imagem em tamnho real
+    </a>
+    </div>
+`).appendTo(novoItem);
 $(novoItem).appendTo('ul');
 $(novoItem).fadeIn(1000);
 $('#endereco-imagem-nova').val('');
